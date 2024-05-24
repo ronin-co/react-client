00ed4f4c65c6e82d2dcf40e476a8a2b054dd0a87
Leo Lamprecht <leo@ronin.co>
2024-05-24 17:26:30 +0200

    Mark `Image` as client component (#6)
    * Mark `Image` as client component
    
    * Declare directive in file name too
    
    * Retain JSX
    
    * Retain JSX
    
    * Ensure JSX is properly retained
    
    * Ensure working build
    
    * Ensure correct formatting
    
    * Delete unused file

--- src/index.ts
+++ src/index.ts
@@ -2,4 +2,4 @@ export * from "ronin";
 export { default } from "ronin";
 
 export { default as RichText } from "./components/rich-text";
-export { default as Image } from "./components/image";
+export { default as Image } from "./components/image.client";
