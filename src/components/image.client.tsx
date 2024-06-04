2615e7944f68de8e56a89c1482ef1b1c90e14b81
Dilshod Turobov <deebov@yandex.com>
2024-06-04 14:40:21 +0200

    Bring back `jsx: "react"`

--- src/components/image.client.tsx
+++ src/components/image.client.tsx
@@ -1,6 +1,11 @@
 "use client";
 
-import { useCallback, useRef, forwardRef, type CSSProperties } from "react";
+import React, {
+  useCallback,
+  useRef,
+  forwardRef,
+  type CSSProperties,
+} from "react";
 import type { StoredObject } from "ronin/types";
 
 const supportedFitValues = ["fill", "contain", "cover"];
